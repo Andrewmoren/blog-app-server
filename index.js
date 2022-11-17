15c18bb8f79984a750a5a378296617faf32a6aab
Andrew <andrewmoren93@gmail.com>
2022-11-17 07:49:31 +0300

    start project

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send(" Hello World!");
+});
+
+app.post("/auth/login", (req, res) => {
+  res.json({
+    success: true,
+  });
+});
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server Ok");
+});
