e007a7056ddea1e4e866c1c20f338d6fe9652cfc
Andrew <andrewmoren93@gmail.com>
2022-11-17 22:03:14 +0300

    add auth

--- index.js
+++ index.js
@@ -1,12 +1,27 @@
 import express from "express";
+import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import { validationResult } from "express-validator";
+
+import { registerValidation } from "./validations/auth.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:260593@cluster0.zqxltkg.mongodb.net/?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("Error", err));
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(" Hello World!");
-});
+app.use(express.json());
 
-app.post("/auth/login", (req, res) => {
+app.post("/auth/register", registerValidation, (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
+
   res.json({
     success: true,
   });
