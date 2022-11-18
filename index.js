ce4ea9a4253a5c87cd9eb2a65ee91538daca5a01
Andrew <andrewmoren93@gmail.com>
2022-11-18 10:57:35 +0300

    create middleware

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 import express from "express";
-import jwt from "jsonwebtoken";
-import bcrypt from "bcrypt";
+
 import mongoose from "mongoose";
-import { validationResult } from "express-validator";
 
 import { registerValidation } from "./validations/auth.js";
 
-import UserModel from "./models/User.js";
+import checkAuth from "./utils/checkAuth.js";
+import { getMe, login, register } from "./controllers/UserController.js";
 
 mongoose
   .connect(
@@ -19,95 +18,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post("/auth/login", async (req, res) => {
-  try {
-    const user = await UserModel.findOne({
-      email: req.body.email,
-    });
+app.post("/auth/login", login);
 
-    if (!user) {
-      return req.status(404).json({
-        message: "User not found",
-      });
-    }
+app.post("/auth/register", registerValidation, register);
 
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-    if (!isValidPass) {
-      return res.status(400).json({
-        message: "Incorrect login or password",
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({
-      message: "Failed to log in",
-    });
-  }
-});
-
-app.post("/auth/register", registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      "secret123",
-      {
-        expiresIn: "30d",
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    res.json({
-      message: "Failed to register",
-    });
-  }
-});
+app.get("/auth/me", checkAuth, getMe);
 
 app.listen(4444, (err) => {
   if (err) {
